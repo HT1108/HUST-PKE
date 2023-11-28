cb49eb9c0f144668f6b00468c5f566d2b4f99736
ht <641776561@qq.com>
2023-11-28 16:09:51 +0800

    fix error: should set tick_count to 0 before calling schedule()

--- kernel/strap.c
+++ kernel/strap.c
@@ -86,8 +86,9 @@ void rrsched() {
   // TIME_SLICE_LEN (means it has consumed its time slice), change its status into READY,
   // place it in the rear of ready queue, and finally schedule next process to run.
   // panic( "You need to further implement the timer handling in lab3_3.\n" );
-  
-  if (++current->tick_count >= TIME_SLICE_LEN){
+  current->tick_count++ ;
+  if (current->tick_count >= TIME_SLICE_LEN) {
+    current->tick_count = 0;
     insert_to_ready_queue(current);
     schedule();
   }
